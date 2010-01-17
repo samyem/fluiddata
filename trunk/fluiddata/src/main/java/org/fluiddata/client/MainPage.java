a3f9bcee0063b29fb1b477d177cf38706935947e
samyem@gmail.com <samyem@gmail.com@01f5adcd-f71b-0410-a972-63ce0d5a8c97>
2010-01-17 19:39:59 +0000

    browse folder

--- trunk/fluiddata/src/main/java/org/fluiddata/client/MainPage.java
+++ trunk/fluiddata/src/main/java/org/fluiddata/client/MainPage.java
@@ -3,7 +3,10 @@
  */
 package org.fluiddata.client;
 
+import java.util.List;
+
 import org.fluiddata.client.model.Folder;
+import org.fluiddata.client.model.Item;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -16,7 +19,6 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
@@ -27,8 +29,7 @@ import com.google.gwt.user.client.ui.Widget;
  * 
  */
 public class MainPage extends Composite {
-    private static final String loadingIndicator="_L";
-    private RPCServiceAsync service = GWT.create(RPCService.class);
+    private static final String loadingIndicator = "_L";
     private ContainerManagerAsync containerManager = GWT.create(ContainerManager.class);
 
     private static MainPageUiBinder uiBinder = GWT.create(MainPageUiBinder.class);
@@ -40,59 +41,84 @@ public class MainPage extends Composite {
     Button button;
     @UiField
     TextBox baseFolder;
-    @UiField
-    Label label;
-    
+
     @UiField
     Tree tree;
-    
+
+    private String base;
+
     public MainPage(String firstName) {
         initWidget(uiBinder.createAndBindUi(this));
-
-        // Can access @UiField after calling createAndBindUi
-        button.setText(firstName);
-
         tree.addOpenHandler(new OpenHandler<TreeItem>() {
-            
+
             @Override
             public void onOpen(OpenEvent<TreeItem> event) {
-                Window.alert("Tree opened");
-                
-                TreeItem target = event.getTarget();
-                
-                //is it loaded yet?
-                if(target.getChildCount()==1 && target.getChild(0).getUserObject().equals(loadingIndicator)){
-                    Window.alert("Need to load");                    
+                final TreeItem target = event.getTarget();
+
+                // is it loaded yet?
+                if (target.getChildCount() == 1 && target.getChild(0).getUserObject().equals(loadingIndicator)) {
+                    Folder userObject = (Folder) target.getUserObject();
+                    containerManager.getItems(base, userObject.getPath(), new AsyncCallback<List<Item>>() {
+
+                        @Override
+                        public void onSuccess(List<Item> result) {
+                            target.removeItems();
+
+                            for (Item item : result) {
+                                if (item instanceof Folder) {
+                                    addFolder((Folder) item, target);
+                                } else {
+                                    TreeItem treeItem = new TreeItem(item.getName());
+                                    treeItem.setUserObject(item);
+                                    target.addItem(treeItem);
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Throwable caught) {
+                            Window.alert("Error:" + caught.getMessage());
+                        }
+                    });
+
                 }
             }
         });
-        
+
     }
 
     @UiHandler("button")
     void onClick(ClickEvent e) {
-        containerManager.getRootFolder(baseFolder.getText(), new AsyncCallback<Folder>() {
-            
+        base = baseFolder.getText();
+
+        containerManager.getRootFolder(base, new AsyncCallback<Folder>() {
+
             @Override
             public void onSuccess(Folder result) {
-                label.setText(result.getName());
-                
-                
-                TreeItem item=new TreeItem(result.getName());
-
-                TreeItem loadingItem=new TreeItem("Loading...");
-                loadingItem.setUserObject(loadingIndicator);
-                
-                item.addItem(loadingItem);
-                
-                tree.addItem(item);
+                addFolder(result, null);
             }
-            
+
             @Override
             public void onFailure(Throwable caught) {
-                Window.alert("error"+caught);
+                Window.alert("error" + caught);
             }
         });
+    }
+
+    private void addFolder(Folder folder, TreeItem target) {
+        TreeItem item = new TreeItem(folder.getName());
+        item.setUserObject(folder);
+
+        TreeItem loadingItem = new TreeItem("Loading...");
+        loadingItem.setUserObject(loadingIndicator);
+
+        item.addItem(loadingItem);
+
+        if (target == null)
+            tree.addItem(item);
+        else
+            target.addItem(item);
     }
 
 }
